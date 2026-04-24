3c974fbc8f5febb42bbf5f97c16b3ab0123a23ed
Smetank0105 <206859408+Smetank0105@users.noreply.github.com>
2026-04-24 18:17:25 +0700

    search DONE

--- RazorPages/RazorPages/Pages/Students/Index.cshtml.cs
+++ RazorPages/RazorPages/Pages/Students/Index.cshtml.cs
@@ -27,14 +27,22 @@ namespace RazorPages.Pages.Students
 
         public IList<Student> Student { get;set; } = default!;
 
-        public async Task OnGetAsync(string sortOrder)
+        public async Task OnGetAsync(string sortOrder, string searchString)
         //public async Task OnGetAsync()
         {
             NameSort = String.IsNullOrEmpty(sortOrder) ? "name_desc" : "";
             DateSort = sortOrder == "Date" ? "date_desc" : "Date";
 
+            CurrentFilter = searchString;
+
             IQueryable<Student> studentsIQ = from s in _context.Students
                                              select s;
+
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                studentsIQ = studentsIQ.Where(s => s.LastName.Contains(searchString)
+                                       || s.FirstName.Contains(searchString));
+            }
 
             switch (sortOrder)
             {
